70948aa3855f9a8e4f795e3968cc4acb80ad5e5d
vasayShinkar <vasayvasay38@gmail.com>
2024-04-29 20:50:50 +0600

    изменения новые

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,18 +4,66 @@ import User from "./сomponents/user"
 import AddTask from './сomponents/addTask';
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+        userAnswer: ["", ""],
+        users: [
+            {
+                id: 1,
+                question: "2 + 2",
+                answer1: 4,
+                answer2: 5,
+                answer3: 6,
+                current: 4,
+            },
+            {
+                id: 2,
+                question: "Висит груша, но нельзя скушать",
+                answer1: "лампочка",
+                answer2: "боксерская груша",
+                answer3: "Кабачок",
+                current: "лампочка",
+            },
+         ]
 
+    }
+    this.solve = this.solve.bind(this)
+    this.addTask = this.addTask.bind(this)
+
+}
  
  render () { 
     return <div> 
         <h1>Загадки</h1>
         <h3>Инфо: Ответы показываютя автоматически</h3>
-        <User />
-        <hr />
-        Добавление:
-        <AddTask />
+        <div className='container'>
+        <div className="task"><User users={this.state} add={this.solve}/> </div>
+        <div className='form'><AddTask add={this.addTask}/></div>
+
+        </div>
+     
       </div>
       
+  }
+  solve(id, value) {
+
+    const updatedUserAnswer = [...this.state.userAnswer]; 
+    updatedUserAnswer[id] = value
+    this.setState({
+      userAnswer: updatedUserAnswer
+    })
+  }
+
+
+
+
+  addTask(data) {
+
+   const id = this.state.users.length + 1
+   this.setState({users: [...this.state.users, {id, ...data}]})
+   this.setState({userAnswer: [...this.state.userAnswer, ""]})
+  
   }
 
 }
--- my-app/src/сomponents/addTask.js
+++ my-app/src/сomponents/addTask.js
@@ -2,15 +2,34 @@ import React from 'react';
 
 
  class AddTask extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      task: "",
+      answer1: "",
+      answer2: "",
+      answer3: "",
+      solve: "",      
+    }
+  }
    render() { 
      return (
         <form>
-            <input placeholder='Сама загадка'/>
-            <input placeholder='первый  ответ'/>
-            <input placeholder='второй ответ'/>
-            <input placeholder='третий ответ'/>
-            <input placeholder="Верный ответ"/>
-            <button type="button">Добавить</button>
+            <input placeholder='Сама загадка' onChange={(event) => this.setState({task: event.target.value})}/>
+            <input placeholder='первый  ответ' onChange={(event) => this.setState({answer1: event.target.value})}/>
+            <input placeholder='второй ответ' onChange={(event) => this.setState({answer2: event.target.value})}/>
+            <input placeholder='третий ответ' onChange={(event) => this.setState({answer3: event.target.value})}/>
+            <input placeholder="Верный ответ" onChange={(event) => this.setState({solve: event.target.value})}/>
+            <button type="button" onClick={() => {this.props.add(
+              {
+                question: this.state.task,
+                answer1: this.state.answer1,
+                answer2: this.state.answer2,
+                answer3: this.state.answer3,
+                current: this.state.solve,
+            }
+
+          )}}>Добавить</button>
         </form>
      )
    }
